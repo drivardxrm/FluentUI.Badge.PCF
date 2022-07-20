7219904e1b6b3a3599088e892ddb42824c58956b
David Rivard <david.rivard@hiloenergie.com>
2022-07-20 19:27:28 -0400

    Multipart badge

--- FluentUIBadge/FluentUIBadgeApp.tsx
+++ FluentUIBadge/FluentUIBadgeApp.tsx
@@ -1,49 +1,48 @@
-
-// eslint-disable-next-line no-use-before-define
-import { Badge, FluentProvider, teamsDarkTheme, teamsHighContrastTheme, teamsLightTheme, webDarkTheme, webLightTheme } from '@fluentui/react-components'
+import { Badge, FluentProvider, makeStyles, webLightTheme } from '@fluentui/react-components'
 import * as React from 'react'
 import { IInputs } from './generated/ManifestTypes'
 
-// eslint-disable-next-line no-undef
+const useStyles = makeStyles({
+    badge: { 
+        marginLeft: '5px'
+    } 
+});
+
+
 const FluentUIBadgeApp = (context:ComponentFramework.Context<IInputs>): JSX.Element => {
-    const message = context.parameters.message.formatted ?? context.parameters.message.raw
+    
+    const classes = useStyles();
+    const getInputs = ():string[] =>{
+        let inputs:string[] = []
+
+        switch (context.parameters.input.type) {
+            case "MultiSelectOptionSet":
+                (context.parameters.input.formatted)?.split(";").forEach(x => inputs.push(x))
+                break;
+            default:
+                inputs.push(context.parameters.input.formatted ?? context.parameters.input.raw)
+                break;
+        }
+        return inputs
+    } 
     const appearance = context.parameters.appearance.raw ?? 'filled'
     const size = context.parameters.size.raw ?? 'medium'
     const shape = context.parameters.shape.raw ?? 'circular'
     const color = context.parameters.color.raw ?? 'brand'
-    const theme = context.parameters.theme.raw ?? 'webLightTheme'
-    // const redirecturl = context.parameters.url.raw
     
     return (
-    <>
-        { theme == 'webLightTheme' && 
-            <FluentProvider theme={webLightTheme}>
-                <Badge appearance={appearance} size={size} shape={shape} color={color}>{message}</Badge>
-            </FluentProvider>
-        }
-        { theme == 'webDarkTheme' && 
-            <FluentProvider theme={webDarkTheme}>
-                <Badge appearance={appearance} size={size} shape={shape} color={color}>{message}</Badge>
-            </FluentProvider>
-        }
-        { theme == 'teamsDarkTheme' && 
-            <FluentProvider theme={teamsDarkTheme}>
-                <Badge appearance={appearance} size={size} shape={shape} color={color}>{message}</Badge>
-            </FluentProvider>
-        }
-        { theme == 'teamsHighContrastTheme' && 
-            <FluentProvider theme={teamsHighContrastTheme}>
-                <Badge appearance={appearance} size={size} shape={shape} color={color}>{message}</Badge>
-            </FluentProvider>
-        }
-        { theme == 'teamsLightTheme' && 
-            <FluentProvider theme={teamsLightTheme}>
-                <Badge appearance={appearance} size={size} shape={shape} color={color}>{message}</Badge>
-            </FluentProvider>
-        }
-
-    </>
-        
+        <FluentProvider theme={webLightTheme}>
+            {getInputs().map((input) => 
+                <Badge
+                    className={classes.badge}
+                    appearance={appearance} 
+                    size={size} 
+                    shape={shape} 
+                    color={color}>
+                        {input}
+                </Badge>
+            )}   
+        </FluentProvider>
     )
 }
 
--- FluentUIBadge/index.ts
+++ FluentUIBadge/index.ts
@@ -1,4 +1,4 @@
-import React = require("react");
+import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import FluentUIBadgeApp from "./FluentUIBadgeApp";
 import {IInputs, IOutputs} from "./generated/ManifestTypes";
